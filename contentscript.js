55b762ddaf7e358166d4b4c0cb57b0225ec010c7
Chris Matthieu <chris@matthieu.us>
2011-05-06 10:32:47 -0700

    refined regex and fixed innerHTML

--- contentscript.js
+++ contentscript.js
@@ -1,4 +1,8 @@
-var regex = /[01]?[- .]?\(?[2-9]\d{2}\)?[- .]?\d{3}[- .]?\d{4}/g;
+// var regex = /[01]?[- .]?\(?[2-9]\d{2}\)?[- .]?\d{3}[- .]?\d{4}/g;
+// var regex = /1?[- .]?\(?[2-9]\d{2}\)?[- .]?\d{3}[- .]?\d{4}/g;
+// var regex = /[>\s]1?[- \.]?\(?[2-9]\d{2}\)?[- \.]?\d{3}[- \.]?\d{4}[<\s]/g;
+// var regex = /[>\s]1?[- \.]?\(?[2-9]\d{2}\)?[- \.]?\d{3}[- \.]?\d{4}/g;
+var regex = /[\s]1?[- \.]?\(?[2-9]\d{2}\)?[- \.]?\d{3}[- \.]?\d{4}/g;
 var page;
 var hits;
 
@@ -6,25 +10,30 @@ var hits;
 if (regex.test(document.body.innerText)) {
   // The regular expression produced a match, so notify the background page.
 
-	page = document.body.innerText;
+	// page = document.body.innerHTML;
+	page = document.documentElement.innerHTML;
 	
-	hits = page.match(regex)
+	hits = page.match(regex);
 	
 	for (var i = 0; i < hits.length; i += 1) {
 	    // page = page.replace(hits[i], '<a href="http://phonophone.heroku.com?phone=' + trimString(hits[i]) + '">' + hits[i] + '</a>');
 	    page = page.replace(hits[i], '<a href="#" onclick="javascript:window.open(\'http://phonophone.heroku.com?phone=' + trimString(hits[i]) + '\' , \'popup_id\', \'scrollbars,resizable,width=350,height=400\')">' + hits[i] + '</a>');
+	console.log('found: ' + trimString(hits[i]))
 	}
 	
+	document.write (document.doctype);
 	document.write (page);
 	
   	chrome.extension.sendRequest({}, function(response) {});
 
 } else {
   // No match was found.
+  console.log('no matches')
 }
 
 
 //To trim leading or trailing spaces
 function trimString (str) {
-   return str.replace(/^\s+/g, '').replace(/\s+$/g, '');  
+   // return str.replace(/^\s+/g, '').replace(/\s+$/g, '');  
+   return str.replace(/\s+/g, '').replace(/\(/g, '').replace(/\)/g, '').replace(/\./g, '').replace(/\-/g, '');  
 }
